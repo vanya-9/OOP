5150c39e63cbb0cc1c357d6f45bf48014d3f6209
new name <zerolie@mail.ru>
2024-12-11 20:59:46 +0700

    almst

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -1,133 +1,158 @@
 #include <iostream>
 #include <list>
-#include <memory>
 #include <string>
+#include <memory>
 
-template <class T>
+
+template <typename T>
 class IObserver {
  public:
-  virtual ~IObserver() {};
-  virtual void onEvent(const T& event_data) = 0;
+  virtual ~IObserver() {}
+  virtual void onEvent(const T &event_data) = 0;
 };
 
-template <class T>
+template <typename T>
 class ISubject {
  public:
-  virtual ~ISubject() {};
+  virtual ~ISubject() {}
   virtual void Attach(std::shared_ptr<IObserver<T>> observer) = 0;
   virtual void Detach(std::shared_ptr<IObserver<T>> observer) = 0;
-  virtual void Notify() = 0;
+  virtual void Notify(const T& event_data) = 0;
 };
 
-/**
- * Издатель владеет некоторым важным состоянием и оповещает наблюдателей о его
- * изменениях.
- */
-template <class T>
+template <typename T>
 class Subject : public ISubject<T> {
  public:
-  virtual ~Subject() { std::cout << "Goodbye, I was the Subject.\n"; }
+    Subject();
+  virtual ~Subject();
 
-  /**
-   * Методы управления подпиской.
-   */
-  void Attach(std::shared_ptr<IObserver<T>> observer) override {
+  void Attach(std::shared_ptr<IObserver<T>> observer) override;
+  void Detach(std::shared_ptr<IObserver<T>> observer)override;
+  void Notify(const T& event_data)override;
+
+  void CreateEvent(T data_event);
+  void HowManyObserver();
+   void SetData(T data_event);
+
+ private:
+ class SubjectImpl;
+  std::list<std::shared_ptr<IObserver<T>>> list_observer_;
+  
+  std::unique_ptr<SubjectImpl> pimpl;
+};
+
+template <typename T>
+Subject<T>::Subject() : pimpl{std::make_unique<SubjectImpl>()} {}
+
+template <typename T>
+Subject<T>::~Subject() {
+  std::cout << "Goodbye, I was the Subject.\n";
+} 
+
+template <typename T>
+void Subject<T>::Attach(std::shared_ptr<IObserver<T>> observer) {
+    pimpl->Attach(observer);
+}
+
+template <typename T>
+void Subject<T>::Detach(std::shared_ptr<IObserver<T>> observer) {
+    pimpl->Detach(observer);
+}
+
+template <typename T>
+void Subject<T>::SetData(T data_event){
+    pimpl->SetData(data_event);
+}
+
+template <typename T>
+void Subject<T>::Notify(const T& data_event) {
+    pimpl->Notify(data_event);
+}
+
+template<typename T>
+void Subject<T>::CreateEvent(T data_event) {
+    // this->data_ = data_event;
+    pimpl->SetData(data_event);
+    Notify(data_event);
+}
+template<typename T>
+class Subject<T>::SubjectImpl{
+    private:
+    std::list<std::shared_ptr<IObserver<T>>> list_observer_;
+    T data_;
+    public:
+    void Attach(std::shared_ptr<IObserver<T>> observer)  {
     list_observer_.push_back(observer);
   }
-  void Detach(std::shared_ptr<IObserver<T>> observer) override {
+  void Detach(std::shared_ptr<IObserver<T>> observer)  {
     list_observer_.remove(observer);
   }
-  void Notify() override {
+  void Notify(const T& event_data)  {
     HowManyObserver();
-    for (auto& observer : list_observer_) {
-      observer->onEvent(message_);
+    for (auto &observer : list_observer_) {
+      observer->onEvent(event_data);
     }
   }
 
-  void CreateMessage(T message = "Empty") {
-    this->message_ = message;
-    Notify();
+  void CreateEvent(T data_event) {
+    SetData(data_event);
+    Notify(data_);
   }
   void HowManyObserver() {
-    std::cout << "There are " << list_observer_.size()
-              << " observers in the list.\n";
+    std::cout << "There are " << list_observer_.size() << " observers in the list.\n";
+  }
+  
+  void SetData(T data_event){
+    data_ = data_event;
   }
 
- private:
-  std::list<std::shared_ptr<IObserver<T>>> list_observer_;
-  T message_;
+
 };
 
-template <class T>
-class Observer : public IObserver<T>,
-                 public std::enable_shared_from_this<Observer<T>> {
+
+template <typename T>
+class ConsoleObserver : public IObserver<T>, public std::enable_shared_from_this<ConsoleObserver<T>> {
  public:
-  Observer(Subject<T>& subject) : subject_(subject) {
-    // this->subject_.Attach(this->shared_from_this());
-    std::cout << "Hi, I'm the Observer \"" << ++Observer::static_number_
-              << "\".\n";
-    this->number_ = Observer::static_number_;
+  ConsoleObserver(std::shared_ptr<Subject<T>> subject) : subject_(subject) {
+    std::cout << "Hi, I'm the Observer \"" << ++ConsoleObserver::static_number_ << "\".\n";
+    this->number_ = ConsoleObserver::static_number_;
   }
-  virtual ~Observer() {
+  virtual ~ConsoleObserver() {
     std::cout << "Goodbye, I was the Observer \"" << this->number_ << "\".\n";
   }
 
-  void onEvent(const T& event_data) override {
-    message_from_subject_ = event_data;
+  void onEvent(const T &event_data) override {
+    data_ = event_data;
     PrintInfo();
   }
   void RemoveMeFromTheList() {
-    subject_.Detach(this->shared_from_this());
+    subject_->Detach(this->shared_from_this());
     std::cout << "Observer \"" << number_ << "\" removed from the list.\n";
   }
   void PrintInfo() {
-    std::cout << "Observer \"" << this->number_
-              << "\": a new message is available --> "
-              << this->message_from_subject_ << "\n";
+    std::cout << "Observer \"" << this->number_ << "\": EventData --> " << this->data_ << "\n";
   }
 
-  void AttachToSubject() { subject_.Attach(this->shared_from_this()); }
+  void Setting() {
+    subject_->Attach(this->shared_from_this());
+  }
 
  private:
-  T message_from_subject_;
-  Subject<T>& subject_;
+  T data_;
+  std::shared_ptr<Subject<T>> subject_;
   static int static_number_;
   int number_;
 };
 
-template <class T>
-int Observer<T>::static_number_ = 0;
-
 template <typename T>
-std::shared_ptr<Observer<T>> CreateObserver(Subject<T>& subject) {
-  auto observer = std::make_shared<Observer<T>>(subject);
-  observer->AttachToSubject();
-  return observer;
-};
+int ConsoleObserver<T>::static_number_ = 0;
 
 void ClientCode() {
-    auto subject = std::make_shared<Subject<std::string>>();
-
-    auto observer1 = CreateObserver<std::string>(*subject); // T = std::string
-    auto observer2 = CreateObserver<std::string>(*subject); // T = std::string
-    auto observer3 = CreateObserver<std::string>(*subject); // T = std::string
-    std::shared_ptr<Observer<std::string>> observer4;
-    std::shared_ptr<Observer<std::string>> observer5;
-
-    subject->CreateMessage("Hello World! :D");
-    observer3->RemoveMeFromTheList();
-
-    subject->CreateMessage("The weather is hot today! :p");
-    observer4 = CreateObserver<std::string>(*subject); // T = std::string
-
-    observer2->RemoveMeFromTheList();
-    observer5 = CreateObserver<std::string>(*subject); // T = std::string
-
-    subject->CreateMessage("My new car is great! ;)");
-    observer5->RemoveMeFromTheList();
-
-    observer4->RemoveMeFromTheList();
-    observer1->RemoveMeFromTheList();
+  auto subject2 = std::make_shared<Subject<int>>();
+  auto int_observer = std::make_shared<ConsoleObserver<int>>(subject2);
+  int_observer->Setting();
+  subject2->CreateEvent(25);
+  int_observer->RemoveMeFromTheList();
 }
 
 int main() {
