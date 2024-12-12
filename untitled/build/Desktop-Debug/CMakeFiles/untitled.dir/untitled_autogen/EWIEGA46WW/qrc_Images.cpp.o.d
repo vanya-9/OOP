0ab7cb2dd82a249b9f57c9d9edd9c6695f758c3c
new name <zerolie@mail.ru>
2024-12-13 02:20:17 +0700

    Delete

--- untitled/build/Desktop-Debug/CMakeFiles/untitled.dir/untitled_autogen/EWIEGA46WW/qrc_Images.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/untitled.dir/untitled_autogen/EWIEGA46WW/qrc_Images.cpp.o: \
-  /home/admin1/Desktop/untitled/build/Desktop-Debug/untitled_autogen/EWIEGA46WW/qrc_Images.cpp
--- untitled/logic.cpp
+++
@@ -1,4 +0,0 @@
-#include "logic.h"
-int turn_to_walk = 0;
-bool rokirovka = false;
-logic::logic() {}
